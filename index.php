f10fc43e32ec62ee941f4fde95796ff6867eec2b
Micalgenus <root@mail.micalgenus.ml>
2016-11-21 01:19:01 +0900

    backup - Mon Nov 21 01:19:01 KST 2016

--- index.php
+++ index.php
@@ -203,7 +203,7 @@ $("#loadLogout").click(function(){
 			var result = this.responseText;
 			if(result == 100){
 				alert("로그아웃!");
-				document.location.href='index.php';
+				//document.location.href='index.php';
 			}
     }
   };
